250a88093ff19c9a080533ce7ecb846584a12765
Oskar Zegar <104756685+BLAAK1@users.noreply.github.com>
2023-06-06 13:11:52 +0000

    Style change, Layout added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
+import Layout from "./Components/Layout/Layout";
 
 import "./App.css";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>  
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
