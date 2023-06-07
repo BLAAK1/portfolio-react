b45063e044b9dc22cfd476965bba03cf09e9a18a
Oskar Zegar <104756685+BLAAK1@users.noreply.github.com>
2023-06-07 12:20:44 +0000

    Add burder-menu for mobile

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,14 +1,47 @@
 import Navbar from "../Navbar/Navbar";
 import Aboutme from "../AboutMe/AboutMe";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import {Link, Outlet } from "react-router-dom";
+import menuIcon from "../../assets/icons/menu.svg";
+
+import React, { useState } from 'react';
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+
 import "./Layout.css";
 
 export default function Layout() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="app">
-      <Navbar />
+      
+      <Navbar><Button onClick={() => setVisible(true)}><img src={menuIcon} alt="Menu" /></Button></Navbar>
       <div className="mainContent">
+        <div className="sidebar">
+        <Sidebar visible={visible} position="left" onHide={() => setVisible(false)}>
+                <div class="hiddenMenu hidden">
+                  <div class="hiddenMenuList">
+                    <ul>
+                      <li class="hiddenExperience hiddenEl" onClick={() => setVisible(false)}>
+                        <Link to="experience">
+                          <h2>DOŚWIADCZENIE</h2>
+                        </Link>
+                      </li>
+                      <li class="hiddenProjects hiddenEl" onClick={() => setVisible(false)}>
+                        <Link to="projects">
+                          <h2>PROJEKTY</h2>
+                        </Link>
+                      </li>
+                      <li class="hiddenContact hiddenEl" onClick={() => setVisible(false)}>
+                         <Link to="contact">
+                          <h2>KONTAKT</h2>
+                    </Link>
+                      </li>
+                    </ul>
+                </div>
+           </div>
+            </Sidebar>
+        </div>
         <Aboutme />
         <div className="mainContent__right">
           <div className="mainContent__content">
@@ -20,3 +53,5 @@ export default function Layout() {
     </div>
   );
 }
+
+            
