4de6e3934bd170a71594b22abe4f16b0f24edf68
Oskar Zegar <104756685+BLAAK1@users.noreply.github.com>
2023-06-12 13:26:09 +0000

    Slidebar fix

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,44 +5,52 @@ import { Link, Outlet } from "react-router-dom";
 import menuIcon from "../../assets/icons/menu.svg";
 
 import React, { useState } from "react";
-import { Sidebar } from "primereact/sidebar";
-import { Button } from "primereact/button";
 
 import "./Layout.css";
 
 export default function Layout() {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState("none");
   return (
     <div className="app">
-      <div className="sidebar">
-        <Sidebar visible={visible} position="left" onHide={() => setVisible(false)}>
-          <div class="hiddenMenu hidden">
-            <div class="hiddenMenuList">
-              <ul>
-                <li class="hiddenExperience hiddenEl" onClick={() => setVisible(false)}>
-                  <Link to="experience">
-                    <h2>DOŚWIADCZENIE</h2>
-                  </Link>
-                </li>
-                <li class="hiddenProjects hiddenEl" onClick={() => setVisible(false)}>
-                  <Link to="projects">
-                    <h2>PROJEKTY</h2>
-                  </Link>
-                </li>
-                <li class="hiddenContact hiddenEl" onClick={() => setVisible(false)}>
-                  <Link to="contact">
-                    <h2>KONTAKT</h2>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+      <div className="sidebar" style={{ display: visible }}>
+        <div class="hiddenMenu hidden">
+          <div class="hiddenMenuList">
+            <ul>
+              <li>
+                <button onClick={() => setVisible("none")}>&#10006;</button>
+              </li>
+              <li
+                class="hiddenExperience hiddenEl"
+                onClick={() => setVisible("none")}
+              >
+                <Link to="experience">
+                  <h2>DOŚWIADCZENIE</h2>
+                </Link>
+              </li>
+              <li
+                class="hiddenProjects hiddenEl"
+                onClick={() => setVisible("none")}
+              >
+                <Link to="projects">
+                  <h2>PROJEKTY</h2>
+                </Link>
+              </li>
+              <li
+                class="hiddenContact hiddenEl"
+                onClick={() => setVisible("none")}
+              >
+                <Link to="contact">
+                  <h2>KONTAKT</h2>
+                </Link>
+              </li>
+            </ul>
           </div>
-        </Sidebar>
+        </div>
       </div>
       <Navbar>
-        <Button onClick={() => setVisible(true)}>
+        <button id="menu" onClick={() => setVisible("block")}>
           <img src={menuIcon} alt="Menu" />
-        </Button>
+        </button>
       </Navbar>
       <div className="mainContent">
         <Aboutme />
