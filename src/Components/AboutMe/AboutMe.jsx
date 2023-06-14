1f55d422cb46f85354a556654818872891d204b2
Oskar Zegar <104756685+BLAAK1@users.noreply.github.com>
2023-06-14 06:37:38 +0000

    Update 5 files

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,26 +1,26 @@
 import me from "../../assets/placeholderImg.jpeg";
 import "./AboutMe.css";
 
-export default function AboutMe(){
-    return(
-        <div className="mainContent--left">
-          <div className="content">
-            <div>
-              <h2>Oskar Zegar</h2>
-              <p>Aspirujący FullStack Deweloper</p>
-            </div>
-            <div className="myImg">
-              <img src={me} alt="Me" />
-            </div>
-            <div>
-              <ul>
-                <li>Imię: Oskar</li>
-                <li>Nazwisko: Zegar</li>
-                <li>Wiek: 20</li>
-                <li>Lokalizacja: Dębica, Polska</li>
-              </ul>
-            </div>
-          </div>
+export default function AboutMe() {
+  return (
+    <div className="mainContent--left">
+      <div className="content">
+        <div>
+          <h2>Oskar Zegar</h2>
+          <p>Aspirujący FullStack Deweloper</p>
         </div>
-    )
+        <div className="myImg">
+          <img src={me} alt="Me" />
+        </div>
+        <div className="list">
+          <ul>
+            <li>Imię: Oskar</li>
+            <li>Nazwisko: Zegar</li>
+            <li>Wiek: 20</li>
+            <li>Lokalizacja: Dębica, Polska</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
